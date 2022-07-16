6855482d9d1896fe920ba408c93f1a6abf4a7acb
Awais shabir <awais.shabir@cloudsolutions.com.sa>
2022-07-16 16:52:27 +0300

    ShouldThrowValidationExceptionOnModifyIfProductIsInvalidAndLogItAsync -> FAIL

--- Smart/Smart.Api.Tests.Unit/Services/Foundations/Products/ProductServiceTests.Validations.Modify.cs
+++ Smart/Smart.Api.Tests.Unit/Services/Foundations/Products/ProductServiceTests.Validations.Modify.cs
@@ -48,5 +48,73 @@ namespace Smart.Api.Tests.Unit.Services.Foundations.Products
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfProductIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given 
+            var invalidProduct = new Product
+            {
+                //Name = invalidText,
+            };
+
+            var invalidProductException = new InvalidProductException();
+
+            invalidProductException.AddData(
+                key: nameof(Product.Id),
+                values: "Id is required");
+
+            //invalidProductException.AddData(
+            //    key: nameof(Product.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the Product model
+
+            invalidProductException.AddData(
+                key: nameof(Product.CreatedDate),
+                values: "Date is required");
+
+            invalidProductException.AddData(
+                key: nameof(Product.CreatedByUserId),
+                values: "Id is required");
+
+            invalidProductException.AddData(
+                key: nameof(Product.UpdatedDate),
+                values: "Date is required");
+
+            invalidProductException.AddData(
+                key: nameof(Product.UpdatedByUserId),
+                values: "Id is required");
+
+            var expectedProductValidationException =
+                new ProductValidationException(invalidProductException);
+
+            // when
+            ValueTask<Product> modifyProductTask =
+                this.productService.ModifyProductAsync(invalidProduct);
+
+            ProductValidationException actualProductValidationException =
+                await Assert.ThrowsAsync<ProductValidationException>(
+                    modifyProductTask.AsTask);
+
+            //then
+            actualProductValidationException.Should().BeEquivalentTo(expectedProductValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedProductValidationException))),
+                        Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateProductAsync(It.IsAny<Product>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
