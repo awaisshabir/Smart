2eed9f251a9fbd523b032474454b54e36ff08bfd
Awais shabir <awais.shabir@cloudsolutions.com.sa>
2022-07-16 16:52:19 +0300

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> PASS

--- Smart/Smart.Api.Tests.Unit/Services/Foundations/Products/ProductServiceTests.Exceptions.RetrieveById.cs
+++ Smart/Smart.Api.Tests.Unit/Services/Foundations/Products/ProductServiceTests.Exceptions.RetrieveById.cs
@@ -53,5 +53,48 @@ namespace Smart.Api.Tests.Unit.Services.Foundations.Products
             this.loggingBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveByIdIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someId = Guid.NewGuid();
+            var serviceException = new Exception();
+
+            var failedProductServiceException =
+                new FailedProductServiceException(serviceException);
+
+            var expectedProductServiceException =
+                new ProductServiceException(failedProductServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectProductByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<Product> retrieveProductByIdTask =
+                this.productService.RetrieveProductByIdAsync(someId);
+
+            ProductServiceException actualProductServiceException =
+                await Assert.ThrowsAsync<ProductServiceException>(
+                    retrieveProductByIdTask.AsTask);
+
+            // then
+            actualProductServiceException.Should()
+                .BeEquivalentTo(expectedProductServiceException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectProductByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+               broker.LogError(It.Is(SameExceptionAs(
+                   expectedProductServiceException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
