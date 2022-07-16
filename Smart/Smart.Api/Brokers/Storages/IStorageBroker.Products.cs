72e6285727fb8926830b24ab85c60ff4d5e3ed88
Awais shabir <awais.shabir@cloudsolutions.com.sa>
2022-07-16 17:04:11 +0300

    BROKERS: Storage - Insert Product

---
+++ Smart/Smart.Api/Brokers/Storages/IStorageBroker.Products.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Smart.Api.Models.Products;
+
+namespace Smart.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Product> InsertProductAsync(Product product);
+    }
+}
