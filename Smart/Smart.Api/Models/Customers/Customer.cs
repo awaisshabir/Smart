6b727a70f8dc4613c5d24dd8810db59b7219444e
Awais shabir <awais.shabir@cloudsolutions.com.sa>
2022-07-16 16:49:47 +0300

    BROKERS: DateTime - Get Current DateTime Offset

---
+++ Smart/Smart.Api/Models/Customers/Customer.cs
@@ -0,0 +1,12 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) awais. All rights reserved.
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+namespace Smart.Api.Models.Customers
+{
+    public class Customer
+    {
+    }
+}
