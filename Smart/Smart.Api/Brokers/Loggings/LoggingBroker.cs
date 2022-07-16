5abdf5557b73ceb13337f7e53659d12e633d9b51
Awais shabir <awais.shabir@cloudsolutions.com.sa>
2022-07-16 16:50:41 +0300

    BROKERS: Logging - Log LogWarning

--- Smart/Smart.Api/Brokers/Loggings/LoggingBroker.cs
+++ Smart/Smart.Api/Brokers/Loggings/LoggingBroker.cs
@@ -18,5 +18,8 @@ namespace Smart.Api.Brokers.Loggings
 
         public void LogDebug(string message) =>
             this.logger.LogDebug(message);
+
+        public void LogWarning(string message) =>
+            this.logger.LogWarning(message);
     }
 }
