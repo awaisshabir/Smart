2586ca9b995c7fbef160a58c6840b0ea4b57aa07
Awais shabir <awais.shabir@cloudsolutions.com.sa>
2022-07-16 16:52:36 +0300

    ShouldThrowValidationExceptionOnModifyIfReferenceErrorOccursAndLogItAsync -> PASS

--- Smart/Smart.Api.Tests.Unit/Services/Foundations/Products/ProductServiceTests.Exceptions.Modify.cs
+++ Smart/Smart.Api.Tests.Unit/Services/Foundations/Products/ProductServiceTests.Exceptions.Modify.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
 using Moq;
@@ -54,6 +55,60 @@ namespace Smart.Api.Tests.Unit.Services.Foundations.Products
 
             this.storageBrokerMock.Verify(broker =>
                 broker.UpdateProductAsync(randomProduct),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async void ShouldThrowValidationExceptionOnModifyIfReferenceErrorOccursAndLogItAsync()
+        {
+            // given
+            Product someProduct = CreateRandomProduct();
+            string randomMessage = GetRandomMessage();
+            string exceptionMessage = randomMessage;
+
+            var foreignKeyConstraintConflictException =
+                new ForeignKeyConstraintConflictException(exceptionMessage);
+
+            var invalidProductReferenceException =
+                new InvalidProductReferenceException(foreignKeyConstraintConflictException);
+
+            ProductDependencyValidationException expectedProductDependencyValidationException =
+                new ProductDependencyValidationException(invalidProductReferenceException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset())
+                    .Throws(foreignKeyConstraintConflictException);
+
+            // when
+            ValueTask<Product> modifyProductTask =
+                this.productService.ModifyProductAsync(someProduct);
+
+            ProductDependencyValidationException actualProductDependencyValidationException =
+                await Assert.ThrowsAsync<ProductDependencyValidationException>(
+                    modifyProductTask.AsTask);
+
+            // then
+            actualProductDependencyValidationException.Should()
+                .BeEquivalentTo(expectedProductDependencyValidationException);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectProductByIdAsync(someProduct.Id),
+                    Times.Never);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedProductDependencyValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateProductAsync(someProduct),
                     Times.Never);
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
