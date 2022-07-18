a387f027c30647ea216cb02c875364fae6c5a51f
Awais shabir <awais.shabir@cloudsolutions.com.sa>
2022-07-18 16:09:34 +0300

    BROKERS: Storage - Insert Suppliers

---
+++ Smart/Smart.Api/Brokers/Storages/StorageBroker.Supplier.Configurations.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Smart.Api.Models.Supplier;
+
+namespace Smart.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        private static void AddSuppliersConfigurations(ModelBuilder modelBuilder)
+        {
+            // TODO: Add your fluent configurations here
+            // TODO: Call this method from StorageBroker.cs OnModelCreating method
+        }
+    }
+}
---
+++ Smart/Smart.Api/Brokers/Storages/StorageBroker.Supplier.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Smart.Api.Models.Supplier;
+
+namespace Smart.Api.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public DbSet<Suppliers> Supplier { get; set; }
+
+        public async ValueTask<Suppliers> InsertSuppliersAsync(Suppliers suppliers)
+        {
+            using var broker =
+                new StorageBroker(this.configuration);
+
+            EntityEntry<Suppliers> suppliersEntityEntry =
+                await broker.Supplier.AddAsync(suppliers);
+
+            await broker.SaveChangesAsync();
+
+            return suppliersEntityEntry.Entity;
+        }
+    }
+}
