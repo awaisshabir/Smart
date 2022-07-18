ce27fd9b035998f39b4c996241048a3309918b7a
Awais shabir <awais.shabir@cloudsolutions.com.sa>
2022-07-18 16:12:50 +0300

    ShouldThrowValidationExceptionOnModifyIfCustomersIsInvalidAndLogItAsync -> FAIL

--- Smart/Smart.Api.Tests.Unit/Services/Foundations/Customer/CustomersServiceTests.Validations.Modify.cs
+++ Smart/Smart.Api.Tests.Unit/Services/Foundations/Customer/CustomersServiceTests.Validations.Modify.cs
@@ -48,5 +48,73 @@ namespace Smart.Api.Tests.Unit.Services.Foundations.Customer
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.storageBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfCustomersIsInvalidAndLogItAsync(string invalidText)
+        {
+            // given 
+            var invalidCustomers = new Customers
+            {
+                //Name = invalidText,
+            };
+
+            var invalidCustomersException = new InvalidCustomersException();
+
+            invalidCustomersException.AddData(
+                key: nameof(Customers.Id),
+                values: "Id is required");
+
+            //invalidCustomersException.AddData(
+            //    key: nameof(Customers.Name),
+            //    values: "Text is required");
+
+            // TODO: Add or remove data here to suit the validation needs for the Customers model
+
+            invalidCustomersException.AddData(
+                key: nameof(Customers.CreatedDate),
+                values: "Date is required");
+
+            invalidCustomersException.AddData(
+                key: nameof(Customers.CreatedByUserId),
+                values: "Id is required");
+
+            invalidCustomersException.AddData(
+                key: nameof(Customers.UpdatedDate),
+                values: "Date is required");
+
+            invalidCustomersException.AddData(
+                key: nameof(Customers.UpdatedByUserId),
+                values: "Id is required");
+
+            var expectedCustomersValidationException =
+                new CustomersValidationException(invalidCustomersException);
+
+            // when
+            ValueTask<Customers> modifyCustomersTask =
+                this.customersService.ModifyCustomersAsync(invalidCustomers);
+
+            CustomersValidationException actualCustomersValidationException =
+                await Assert.ThrowsAsync<CustomersValidationException>(
+                    modifyCustomersTask.AsTask);
+
+            //then
+            actualCustomersValidationException.Should().BeEquivalentTo(expectedCustomersValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedCustomersValidationException))),
+                        Times.Once());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateCustomersAsync(It.IsAny<Customers>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
