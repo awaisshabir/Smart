4593daab63ae53309c8be5e19f423475b83a2fc2
Awais shabir <awais.shabir@cloudsolutions.com.sa>
2022-07-18 16:09:13 +0300

    BROKERS: Storage - Insert Suppliers

---
+++ Smart/Smart.Api/Brokers/Storages/IStorageBroker.Supplier.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Smart.Api.Models.Supplier;
+
+namespace Smart.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Suppliers> InsertSuppliersAsync(Suppliers suppliers);
+    }
+}
